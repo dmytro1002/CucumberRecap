ab8a02644437e2aab4b9aef3459711e42d839a57
mindtek <dmytrokostiuk13@gmail.com>
2023-05-09 14:52:47 -1000

    Completed place order automation with Examples tabe

--- src/test/java/steps/PizzaAppSteps.java
+++ src/test/java/steps/PizzaAppSteps.java
@@ -4,6 +4,7 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import pages.PizzaAppPage;
@@ -17,6 +18,7 @@ public class PizzaAppSteps {
 
     WebDriver driver = Driver.getDriver();
     PizzaAppPage pizzaAppPage = new PizzaAppPage();
+    String costValue;
 
     @Given("user navigates to {string} application")
     public void user_navigates_to_application(String url) {
@@ -25,23 +27,39 @@ public class PizzaAppSteps {
 
 
     @When("user creates pizza order with data")
-    public void user_creates_pizza_order_with_data(DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
+    public void user_creates_pizza_order_with_data(DataTable dataTable) throws InterruptedException {
         // For automatic transformation, change DataTable to one of
         // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>.
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        /*
+        String strNum="1234";
+        int myInt = Integer.parseInt(strNum);
+         */
+        String str1 ="Banana"; // - literal
+        String str2 ="Banana";
 
-        Map<String, String> data = dataTable.asMap(String.class,String.class);
-        System.out.println(data);
-
-        BrowserUtils.selectDropdownByValue(pizzaAppPage.pizzaDropdown,data.get("Pizza"));
-        BrowserUtils.selectDropdownByValue(pizzaAppPage.topping1Dropdown,data.get("Topping1"));
-        BrowserUtils.selectDropdownByValue(pizzaAppPage.topping2Dropdown,data.get("Topping2"));
-        pizzaAppPage.pizzaQtyBox.sendKeys(Keys.BACK_SPACE+data.get("Quantity"));
-        pizzaAppPage.nameBox.sendKeys(data.get("Name"));
-        pizzaAppPage.emailBox.sendKeys(data.get("Email"));
-        pizzaAppPage.phoneBox.sendKeys(data.get("Phone"));
-        if (data.get("Payment").equals("Cash on Pickup")){
+        //{Pizza=Small 6 Slices - no toppings, Topping1=Mushrooms, Topping2=Extra cheese, Quantity=1,
+        // Name=Patel Harsh, Email=patel@gmail.com, Phone=123456789, Payment=Cash on Pickup} - Map<String,String>
+        Map<String,String> data = dataTable.asMap(String.class,String.class);
+        System.out.println(data.toString());
+        for (String value: data.values()){
+            System.out.println(value);
+        }
+        BrowserUtils.selectDropdownByValue(pizzaAppPage.pizzaDropdown,data.get("Pizza")); //selecting value from the Pizza dropdown
+        BrowserUtils.selectDropdownByValue(pizzaAppPage.topping1Dropdown,data.get("Topping1")); // Mushrooms
+        BrowserUtils.selectDropdownByValue(pizzaAppPage.topping2Dropdown,data.get("Topping2")); // Extra Cheese
+//        pizzaAppPage.pizzaQtyBox.sendKeys(Keys.CONTROL+"A");
+//        pizzaAppPage.pizzaQtyBox.sendKeys(Keys.BACK_SPACE);
+        pizzaAppPage.pizzaQtyBox.sendKeys(Keys.BACK_SPACE);
+//        pizzaAppPage.pizzaQtyBox.clear();
+        pizzaAppPage.pizzaQtyBox.sendKeys(data.get("Quantity")); // "1"
+        pizzaAppPage.nameBox.sendKeys(data.get("Name")); // "Patel Harsh"
+        pizzaAppPage.emailBox.sendKeys(data.get("Email"));// "patel@gmail.com
+        pizzaAppPage.phoneBox.sendKeys(data.get("Phone")); //"123456789"\
+        costValue = pizzaAppPage.pizzaCostBox.getAttribute("value");
+        // String costValue = "6.75";
+        if(data.get("Payment").equals("Cash on Pickup")){
             pizzaAppPage.cashRadioButton.click();
         }else {
             pizzaAppPage.creditCardRadioButton.click();
@@ -49,8 +67,13 @@ public class PizzaAppSteps {
         pizzaAppPage.placeOrderButton.click();
     }
 
-    @Then("user validates that order is created with message {string}")
-    public void user_validates_that_order_is_created_with_message(String string) {
+
+    @Then("user validates that order is created with message {string} {string} {string}")
+    public void userValidatesThatOrderIsCreatedWithMessage(String success, String quantity, String pizza) {
+        String expectedMessage = success+costValue+" "+pizza;
+        String actualMessage = pizzaAppPage.dialogWindow.getText();
+        System.out.println(actualMessage);
+        Assert.assertEquals(expectedMessage,actualMessage);
 
     }
 }
--- src/test/resources/features/PizzaApp.feature
+++ src/test/resources/features/PizzaApp.feature
@@ -2,16 +2,23 @@
 Feature:  Pizza application place order functionality
 
   @RTB-15 @POF-1
-  Scenario: Validating successful place order
+  Scenario Outline: Validating successful place order
     Given user navigates to "PizzaAppURL" application
     When user creates pizza order with data
-      | Pizza    | Small 6 Slices - no toppings |
-      | Topping1 | Mushrooms                    |
-      | Topping2 | Extra cheese                 |
-      | Quantity | 1                            |
-      | Name     | Patel Harsh                  |
-      | Email    | patel@gmail.com              |
-      | Phone    | 1234456789                   |
-      | Payment  | Cash on Pickup               |
-    Then user validates that order is created with message "Thank you for your order! TOTAL: 6.75 Small 6 Slices - no toppings"
+      | Pizza    | <Pizza>    |
+      | Topping1 | <Topping1> |
+      | Topping2 | <Topping2> |
+      | Quantity | <Quantity> |
+      | Name     | <Name>     |
+      | Email    | <Email>    |
+      | Phone    | <Phone>    |
+      | Payment  | <Payment>  |
+    Then user validates that order is created with message "Thank you for your order! TOTAL: " "<Quantity>" "<Pizza>"
+
+    Examples:
+      | Pizza                        | Topping1  | Topping2     | Quantity | Name        | Email           | Phone     | Payment        |
+      | Small 6 Slices - no toppings | Mushrooms | Extra cheese | 1        | Patel Harsh | patel@gmail.com | 123456789 | Cash on Pickup |
+      | Large 10 Slices - 2 toppings | Mushrooms | Extra cheese | 3        | John Doe    | john@gmail.com  | 123456789 | Credit Card    |
+      | Medium 8 Slices - 2 toppings | Olives    | Salami       | 2        | Kim Yan     | kim@gmail.com   | 123456789 | Cash on Pickup |
+
 
